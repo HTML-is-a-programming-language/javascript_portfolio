351ba7e104975bf14438f75d4ce755019d0a205a
seungjunseo <newworld1423@naver.com>
2024-02-27 14:08:01 +0900

    이미지 추가

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// 헤더 로고 이벤트
-document.querySelector('.logo-box').addEventListener('mouseenter', function(){
-    document.querySelector('.door').style.backgroundColor = 'white';
-});
-
-document.querySelector('.logo-box').addEventListener('mouseleave', function(){
-    document.querySelector('.door').style.backgroundColor = 'black';
-});
-
