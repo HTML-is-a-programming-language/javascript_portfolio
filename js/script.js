3508beaf1417b6e35d41832a07453f722d8999f4
seungjunseo <newworld1423@naver.com>
2024-02-27 13:27:02 +0900

    헤더 수정

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-document.getElementsByClassName('door').style.backgroundColor = 'white';
+// 헤더 로고 이벤트
+document.querySelector('.logo-box').addEventListener('mouseenter', function(){
+    document.querySelector('.door').style.backgroundColor = 'white';
+});
+
+document.querySelector('.logo-box').addEventListener('mouseleave', function(){
+    document.querySelector('.door').style.backgroundColor = 'black';
+});
+
